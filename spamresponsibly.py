de8b2d862eb2b4be193941f669d40b392bd65d6e
Kiwi Singh <metal.garuruwind.tetusaiga223@gmail.com>
2023-02-22 21:20:51 +0100

    Added error handling and refactored code
    All credits go to my wonderful friend for kindly taking out the time to refactor my code to use error handling and a main function.

--- spamresponsibly.py
+++ spamresponsibly.py
@@ -1,9 +1,24 @@
-val = int(input(print("Enter a number: ")))
-option = {
-    "n":"Nae nigga nae \n", "c":"Uohhhhhh :sob::sob::sob: \n",
-    "i":"I am living in your walls, oomfie. \n"
+def main():
+    option = {
+        "n":"Nae nikka nae nae nikka nikka nae nae\n", 
+        "c":"Uohhhhhh :sob::sob::sob:\n",
+        "i":"I am living in your walls, oomfie.\n"
     }
-userinput = input("Choose your poison: [N]iggas, [C]unny, [I]solation")
-with open("outputtings.txt",'w') as f:
-    f.write(option[userinput.lower()] * val)
-print("Output saved to file. Spam responsibly, fam.")
+
+    try:
+        val = int(input('Enter a number: '))
+        userinput = input('Choose your poison: [N]ae nae, [C]unny, [I]solation: ')
+
+        if userinput.lower() in ['n', 'c', 'i']:
+            with open("outputtings.txt",'w') as f:
+                f.write(option[userinput.lower()] * val)
+
+            print("Output saved to file. Spam responsibly, fam.")
+        else:
+            print('Dis no work m8, only N, C and I.')
+    except ValueError as ve:
+        print('Dis no numbar plis fix.')
+
+
+if __name__ == '__main__':
+    main()
