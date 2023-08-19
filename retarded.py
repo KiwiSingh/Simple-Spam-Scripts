a107f13ed88d314cf51718f88b207a815a9f1e6c
Kiwi Singh <kiwilovesbig@boo.ba>
2023-08-19 22:11:20 +0200

    Update retarded.py
    Added new copypastas, and made script platform-agnostic

--- retarded.py
+++ retarded.py
@@ -1,7 +1,6 @@
 
-# Copypastas by ChaR1ot33r#6969, cat#0001 and u/GerardDG on Reddit. Script written by ChaR1ot33r#6969. If you kang, you big gei.
-import subprocess
-import platform
+# Copypastas by char1ot33r (on Discord), cat#0001 and u/GerardDG on Reddit. Script written by char1ot33r, with a little help from bababooey_fr (on Discord). If you kang, you big gei.
+import pyperclip
 opt = {
 
     "v":"""
@@ -10,11 +9,44 @@ opt = {
     It’s late, you’ve been in bed sleeping for a few hours. I crawl under the covers. I always sleep naked, so the cool sheets give my whole body goosebumps. I snuggle on my side into the blankets and shiver.\n\nSuddenly I feel you behind me, pressing your warm body to mine, wrapping your arms tight around me. You rub my skin firmly, warming me. Your hand makes its way to my hip and pulls me close. Your cock is hard and throbbing; pulsing between my ass cheeks. I push back, wiggling my hips. Your other hand moves up my body and moves around my neck. You gently squeeze and tilt my head back. You start to grip my neck tighter. You begin to whisper sweet and dirty things in my ear; telling me how you can’t wait to fill me, how badly you want me, how your cock will fit perfectly in my tight pussy.\n \nYour hand on my hip slides down between my legs. You touch my wet pussy, but I can tell you’re not satisfied. You know how to fix that though. You grip my throat tight and whisper:\n\n“Baby…make Daddy proud”\n\nI can feel my pussy soak your fingers when you say that. You know I love it when you’re my Daddy. You chuckle and moan your approval.\n\n“Mmm. My baby girl needs her Daddy to fill her pussy. Are you going to be good and take your Daddy’s big cock?”\n\nAs you say that your fingers slide deep into my pussy. I try to scream, but your hand on my throat muffles the sound. I grind back to try to find release. You’re right - I am so turned on when you take control of my body. I push back on your fingers and start to thrust. I’m desperate to cum for you; to make my Daddy proud. I can feel you smile as I wantonly fuck your fingers.\n\n“That’s right. Be a good girl and cum for your Daddy.”\n\nI let out a moan. My whole body tenses up and my back arches as I gush on your fingers. My pussy clenches and spasms, trying to find what it desperately needs: my Daddy’s hard cock to stuff it full of cum. You sense my need and chuckle in my ear.\n\n“Come on my greedy little slut. Show your Daddy how bad you need it.”\n\nI roll towards you on my other side and wrap my leg around your waist. My hand goes between our legs and grips your hard cock. I can feel precum oozing from the tip. I play with your cock and guide it between my legs where my spread, swollen pussy is waiting for you. I thrust forward so that the tip of your cock is touching my clit. I guide you around, smearing my clit with your precum. Your hand moves to my ass and forcefully spreads me apart, causing the tip of your cock to slip into my wet pussy. I clench around you, trying to draw you in deeper. The penetration is enough to drive me wild. I’m so close to cumming for you, but I need more.\n\nWithout warning you wrap your arms tight around me, pin me to your chest, and roll on to your back. You grip my hair and kiss me deeply. The tip of your cock is teasing my pussy. I’m trying to grind my hips to take you in. You move your hands down to my ass and give me a sharp swat. I yelp in pain and surprise. You use your hands to spread my ass wide as you tell me,\n\n“Baby, put it in. Bounce on your Daddy’s cock.”\n\nI don’t waste a moment. I slide down your thick shaft and stay there a moment, savoring the feeling of being full of your cock. It’s short-lived as I quickly feel the sting of your hand on my ass.\n\n“Bounce baby girl. Make Daddy drain his balls in your hungry little pussy.”\n\nI immediately start sliding up and down your thick cock. I can feel my cum dripping down your shaft. I feel my pussy lips spread and stretch around your cock as I start to fuck you. My pussy is so wet I can hear it each time I thrust your cock deep. My tits bounce as I ride you. Your fingers dig into my hips. I can feel your cock twitch and throb. You’re so close. I need your cum.\n\nI stop a moment to put my feet under me and raise myself up in a squatting position. I know this is what you really wanted all along; it will make you cum so hard. I start with shallow thrusts that tease your cockhead. I move down with deep, slower thrusts and feel the tip of your cock swell as I do. I smile. With my final thrusts I take all of your cock. I clench my pussy as I lift myself back to the tip and that’s when I feel the spurts of your thick, hot cum. I lower myself back down as you twitch, pushing your cum deep. You hold me there so you can pump me full. I grind to make sure your balls are totally empty. I groan your name as I cum for the last time.\n\nI shift back to my knees and bend forward to collapse on your chest. Your breathing is heavy but starting to slow. You wrap your arms around me and start giving me little kisses all over my neck. You whisper in my ear how much you love me, how you love it when I get so turned on when you talk dirty to me. You gently roll me on my back and start rubbing my sore thighs. You kiss me lovingly, bringing me down as I start to relax and drift to sleep.\n\nAs I start to feel your cum drip from my pussy I think, “I can’t wait until my Daddy fucks me again.”
     ''',"d":"""
     Zwemmen in Bacardi Lemon is eigenlijk helemaal niet chill. Denk maar na. Ten eerste is het zoete plakzooi. Bacardi is 300 percent suiker weet je, het is gewoon vloeibare kauwgom. Als je daarin gaat zwemmen gaat dat enorm kleven.\n\nTen tweede ben je waarschijnlijk dronken voordat je je eerste baantje hebt gezwommen. De alcohol krijg je niet alleen binnen door het door te slikken maar ook via je gehemelte enzo. Het doet pijn aan je ogen en ik gok dat het brandt in je neus. Dronken worden is op zich lachen, maar het belemmert je zwemvermogen. Plus je kan moeilijk bijhouden hoeveel je binnenkrijgt. Dus de kans bestaat dat je jezelf in een coma zuipt en/of dat je in de Bacardi Lemon verdrinkt. Een ignobele dood als je het mij vraagt.\n\nMaar wat dus wel een goeie oplossing is, is als je een Bacardi Lemon sauna maakt. Dan heb je wel de alcohol, maar niet de zoete teringzooi (want die blijft achter na distillatie en de kans op comazuipen is kleiner).
+    """, "i":"""
+    The problem is that , I have considered you all my friends. But you guys took advantage of my kindness and used me to feel better about yourself and feel superior when you guys are more destructive and miserable among yourselves. 
+
+    I entertained you guys for a long time. Without realising the friendship with you have not coming from neutral point of respect and I thought We're friends that's why I maintained a harmony. But not anymore. I won't let you shit talk me anymore
     """, "o":"""
     There was once an unfortunate soul known as Onestone. As is probably evident from his name, he only had one testicle. He was always ostracized and made fun of as a kid. One day, he was drinking at a bar, when he met this cute girl, Beverly Bird, who was a childhood acquaintance. She found his name and condition very quaint, but he felt offended by it, so he got her drunk, took her home, and fucked her till she died. He dumped her body in a vat of acid. Two weeks later, Beverly's cousin, Penny Bird, who had no idea of the ill fate that had befallen her sister, saw Onestone at the same bar. 'Hey Onestone. How's it hanging?' she asked. Onestone, offended by this, got her drunk and took her home. But no matter how brutally he'd fuck her, she just wouldn't die. Why? Because you can't kill two Birds with Onestone.
     """, "b":"""
     Listen you have time to do every shit... Posting shit in watsapp story as well as in insta... When you do these things tab toh you don't have any issues, jab bhi bolo padhai karne ke liye you have several issues... And before telling me that I'm insensitive, look at yourself... And anyway good luck... This is the last text I'm texting you... Tumhara kuch bhi nahi ho sakta trust me...
     Like insensitive questions mai puchti hu aur tum kya ho??  Tumhara toh health crisis bht time se hi chal raha hai... Kabhi bhi I guess thik hone wala nahi hai... Anyway... Go back home... That's the best option...
+    """, "a": """
+    I have finally understood why people meme on men who buy onaholes but don't extend the same harsh judgment to females who buy dildos and vibrators. First of all, this onahole from AliExpress, though decently designed, still feels cheap and flimsy, almost like those toy eggs you used to get at fairs and stuff. I totally get where the inspiration for Tenga eggs comes from now.  However, anyone who calls an onahole or Fleshlight or any of these gizmos designed for use by males as "just like the real thing" have another thing coming. While it is true that I busted one helluva nut from my onahole (more on that later), it in no way, shape or form compares to the experience of human skin, and the overall sexual experience in general. I am now skeptical of sex robots, sex dolls, and any number of degenerate apparatuses designed by lonely hikkikomori NEETs to vent out their pent out sexual frustrations. Also, the vulva on this onahole was too tiny. And the labia and clitoris were the wrong proportions, making me think that the person who designed this has either never had sex, or worse, has an even smaller penis than I do (yikes).
+    Speaking of the sexual experience, I found it very hard to get my member into the vaginal orifice on this thing. Maybe it's because I didn't have lube, but I have come to realize that on a normal human being you have any number of tactile appendages you can grip to help you enter any of their orifices. No such tactile aids on this thing. Pure ass. Also, the caps covering the sleeve are not secure enough, so unless you buy a drawstring bag for this thing, it is extremely likely that you will encounter an awkward situation with airport security, or with anyone who happens to open your bag for random checking, for example at an international train stop. I unironically found it easier to enter the mouth part of this double-sided onahole than I did the vagina. And oh boy, what an experience it was...
+    But was it anything like having sex with a real human being? Fuck no.
+    Vagina: 4/10
+    Mouth: 9/10
+    Sleeve: 0/10
+    Nut busting experience: 7/10
+    Actual orgasm experienced: 10/10
+    Overall aggregate score: 6/10
+    More mid than Morbius. Buyer beware.""", "r": """
+    Step 1: Formation of primary oocyte
+    Occurs at the fetal stage by zygotic meiosis
+    Step 2a: Formation of the immature secondary oocyte
+    Occurs in the ovaries of the female at menarche (during puberty)
+    Step 2b: Formation of spermatozoa inside the testes
+    Multistep process that begins at puberty
+    Step 3: Insemination
+    May occur through copulation (sex) or artificial insemination
+    Step 4: Maturation of the secondary oocyte
+    Occurs when one sperm cell reaches the immature secondary oocyte by swimming up through the Fallopian tubes by means of seminal fluid. The antifertilizin-fertilizin reaction leads to maturation of the secondary oocyte and selective exclusion of other sperm cells. Once the corona radiata is breached, the tail of the sperm replaces the mature secondary oocyte's (now called the ovum) plasma membrane, while the haploid nuclei fuse to form the zygote nucleus
+    Step 5: Interphase
+    The zygote prepares to enter mitotic cell division cycles. This is also when errors in gene duplication, such as nondisjunction can manifest
+    Step 6: Mitotic cell division
+    The zygote divides repeatedly until forms first the morula and then the blastula, until cell differentiation results in gastrulation
+    Step 7: Pregnancy
+    During gastrulation, the mass of cells attaches to the uterus. The placenta forms around the maturing foetus
+    Step 8: Parturition
+    After 3 trimesters, the foetus is ready to be born, and is ejected from the mother's birth canal
     """}
 
 userinput = input("""
@@ -22,18 +54,12 @@ Choose your option:
 [V]elo
 [C]at
 [D]utch
+[I]ncel
 [O]nestone
+[A]liExpress
 [B]engali
+[R]eproduction
 """)
 output = opt[userinput.lower()]
-
-if platform.system() == 'Darwin':
-    # macOS
-    command = 'echo "{}" | pbcopy'.format(output)
-    subprocess.run(command, shell=True)
-    print("Text copied to clipboard. Spam pasta responsibly.")
-elif platform.system() == 'Windows':
-    # Windows; currently beta testing, so may not work as expected
-    command = 'echo "{}" | clip'.format(output)
-    subprocess.run(command, shell=True)
-    print("Text copied to clipboard. Spam pasta responsibly.")
+pyperclip.copy(output)
+print("Text copied to clipboard. Spam pasta responsibly.")
